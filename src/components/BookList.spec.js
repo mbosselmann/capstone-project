18768b0dc26ee5884d9baac56737656d5aeadbdd
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-11 15:08:52 +0200

    Add test for booklist

---
+++ src/components/BookList.spec.js
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import BookList from './BookList'
+import placeholder from '../images/placeholder.png'
+
+describe('Book list', () => {
+  const books = [
+    {
+      id: '1',
+      volumeInfo: {
+        title: 'Harry Potter and the Goblet of Fire',
+        authors: ['J. K. Rowling'],
+        imageLinks: {
+          thumbnail: placeholder,
+        },
+      },
+      finished: false,
+      readingSince: '05/2021',
+      finishedSince: '',
+    },
+    {
+      id: '2',
+      volumeInfo: {
+        title: 'Cathedral of the Sea',
+        authors: ['Ildefonso Falcones de Sierra'],
+        imageLinks: {
+          thumbnail: placeholder,
+        },
+      },
+      finished: true,
+      readingSince: '05/2021',
+      finishedSince: '08/2021',
+    },
+  ]
+
+  it('renders two listitems', () => {
+    render(<BookList books={books} />)
+
+    const bookListItems = screen.getAllByRole('listitem')
+    expect(bookListItems).toHaveLength(2)
+  })
+})
