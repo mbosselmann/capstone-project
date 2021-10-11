e8d4ce439f3957d7f7733db4bcb9b552b281e070
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-11 16:29:17 +0200

    Add global styles to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import GlobalStyles from "../src/GlobalStyles"
+
+export const decorators = [
+  (Story) => (
+    <>
+    <GlobalStyles />
+    <Story />
+    </>
+  )
+]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
