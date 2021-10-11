2bbd9d47ce71fe50e4679b248458ad77b42ac9ba
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-11 12:58:29 +0200

    Add book and booklist components, import data for rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <p>Hello World!</p>
+import BookList from './components/BookList'
+import styled from 'styled-components'
+
+function App({ data }) {
+  return (
+    <main>
+      <Header>
+        <h2>List of Books</h2>
+        <h3>Hi user!</h3>
+        <p>Welcome back! Here is your booklist:</p>
+      </Header>
+      <BookList books={data} />
+    </main>
+  )
 }
+
+const Header = styled.header`
+  margin: 20px;
+`
 
 export default App
---
+++ src/components/Book.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+function Book(props) {
+  return (
+    <Wrapper>
+      <Img>
+        <div>
+          <img src={props.bookCover} alt="bookcover" />
+        </div>
+      </Img>
+      <Content>
+        <div>
+          <h3>{props.title}</h3>
+          <p>
+            by
+            {props.authors.length > 1
+              ? ` ${props.authors[0]}`
+              : ` ${props.authors}`}
+          </p>
+        </div>
+        <div>
+          {!props.readingStatus
+            ? `Reading since: ${props.readingStatusDate}`
+            : `
+        Finished since: ${props.finishedSince}`}
+        </div>
+      </Content>
+    </Wrapper>
+  )
+}
+
+export default Book
+
+const Wrapper = styled.li`
+  border-radius: 5px;
+  margin: 20px;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  min-height: 150px;
+  box-shadow: 5px 4px 10px 0px rgba(0, 0, 0, 0.25);
+`
+const Img = styled.div`
+  flex: 1;
+  background-color: #f6f6f6;
+  display: flex;
+
+  div {
+    width: 100%;
+    margin: 10px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div img {
+    width: 60%;
+    box-shadow: 5px 4px 10px 0px rgba(0, 0, 0, 0.25);
+  }
+`
+
+const Content = styled.div`
+  flex: 2;
+  padding: 10px 0 10px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
