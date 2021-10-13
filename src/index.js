6ec8a159a13ad7a329f9bcadb2aeb500560c861d
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-13 14:30:46 +0200

    Add router to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './GlobalStyles'
 import data from './data.json'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App data={data} />
+    <Router>
+      <App data={data} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
