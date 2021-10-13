d9f91727b0a4e178076a1885e70d06c04bb68415
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-13 15:23:52 +0200

    Change tests for booklist component

--- src/components/BookList.spec.js
+++ src/components/BookList.spec.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import BookList from './BookList'
 import placeholder from '../images/placeholder.png'
-import userEvent from '@testing-library/user-event'
 
 describe('Book list', () => {
   const books = [
@@ -34,92 +33,31 @@ describe('Book list', () => {
   ]
 
   it('renders two listitems', () => {
-    const mockOnHandleActiveReadingStatus = jest.fn()
-
-    render(
-      <BookList
-        books={books}
-        handleActiveReadingStatus={mockOnHandleActiveReadingStatus}
-      />
-    )
+    render(<BookList books={books} readingStatus={''} />)
 
     const bookListItems = screen.getAllByRole('listitem')
     expect(bookListItems).toHaveLength(2)
   })
 
-  it('has two buttons', () => {
-    const mockOnHandleActiveReadingStatus = jest.fn()
-
-    render(
-      <BookList
-        books={books}
-        handleActiveReadingStatus={mockOnHandleActiveReadingStatus}
-      />
-    )
-
-    const buttons = screen.getAllByRole('button')
-    expect(buttons).toHaveLength(2)
-  })
-
-  it('renders correct title according to readingStatus= "true"', () => {
-    const mockOnHandleActiveReadingStatus = jest.fn()
-
-    render(
-      <BookList
-        readingStatus={'true'}
-        books={books}
-        handleActiveReadingStatus={mockOnHandleActiveReadingStatus}
-      />
-    )
+  it('renders correct title according to readingStatus="finishedBooks"', () => {
+    render(<BookList books={books} readingStatus={'finishedBooks'} />)
 
     const title = screen.getByRole('heading', { level: 2 })
-    const buttons = screen.getAllByRole('button')
-    const finishedBooksButton = buttons[1]
     expect(title).toBeInTheDocument()
-    userEvent.click(finishedBooksButton)
     const titleFinishedBooks = screen.getByText(
       'Your library of finished books:'
     )
     expect(titleFinishedBooks).toBeInTheDocument()
   })
 
-  it('renders correct title according to readingStatus= "false"', () => {
-    const mockOnHandleActiveReadingStatus = jest.fn()
-
-    render(
-      <BookList
-        readingStatus={'false'}
-        books={books}
-        handleActiveReadingStatus={mockOnHandleActiveReadingStatus}
-      />
-    )
+  it('renders correct title according to readingStatus= "currentlyReading"', () => {
+    render(<BookList books={books} readingStatus={'currentlyReading'} />)
 
     const title = screen.getByRole('heading', { level: 2 })
-    const buttons = screen.getAllByRole('button')
-    const readBooksButton = buttons[0]
     expect(title).toBeInTheDocument()
-    userEvent.click(readBooksButton)
     const titleFinishedBooks = screen.getByText(
       'Your library of books currently read:'
     )
     expect(titleFinishedBooks).toBeInTheDocument()
-  })
-
-  it('renders only one item after click on button', () => {
-    const mockOnHandleActiveReadingStatus = jest.fn()
-
-    render(
-      <BookList
-        readingStatus={'false'}
-        books={books}
-        handleActiveReadingStatus={mockOnHandleActiveReadingStatus}
-      />
-    )
-
-    const buttons = screen.getAllByRole('button')
-    const readBooksButton = buttons[0]
-    userEvent.click(readBooksButton)
-    const bookListItems = screen.getAllByRole('listitem')
-    expect(bookListItems).toHaveLength(1)
   })
 })
