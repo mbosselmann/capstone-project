8e05ce3ac0bc8cfdd8c386fce1abd5b6959a8eac
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-13 14:26:42 +0200

    Add grid-layout

--- src/App.js
+++ src/App.js
@@ -16,6 +16,26 @@ function App({ data }) {
 
 const Header = styled.header`
   margin: 20px;
+  grid-area: header;
+`
+
+const Main = styled.main`
+  grid-area: main;
+  overflow-y: scroll;
+`
+
+const Footer = styled.footer`
+  grid-area: footer;
+  z-index: 10;
+`
+
+const AppContainer = styled.div`
+  height: 100vh;
+  max-width: 600px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-areas: 'header' 'main' 'footer';
+  grid-template-rows: 120px auto 80px;
 `
 
 export default App
