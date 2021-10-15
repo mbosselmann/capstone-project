00789790faba08cc6b769b56a6a1de029e6e4139
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-15 23:48:36 +0200

    Change authorsLength for better readability

--- src/components/Book.js
+++ src/components/Book.js
@@ -12,9 +12,11 @@ function Book({
     if (authors.length === 2) {
       if (authors[0].includes(authors[1])) {
         return ` ${authors[0]}`
+      } else {
+        return ` ${authors[0]} and ${authors[1]}`
       }
-      return ` ${authors[0]} and ${authors[1]}`
-    } else if (authors > 2) {
+    }
+    if (authors > 2) {
       return ` ${authors[0]} and others`
     } else {
       return ` ${authors}`
