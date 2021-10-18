e983e6e7a39a61b2ccabe773347587e5a1716cab
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-18 10:25:06 +0200

    Change and add tests for Navigation

--- src/components/Navigation.spec.js
+++ src/components/Navigation.spec.js
@@ -12,7 +12,7 @@ describe('Navigation', () => {
     )
 
     const links = screen.getAllByRole('link')
-    expect(links).toHaveLength(2)
+    expect(links).toHaveLength(3)
   })
 
   it('changes to active when "currently reading" link is clicked', () => {
@@ -28,7 +28,7 @@ describe('Navigation', () => {
     expect(linkCurrentlyReading).toHaveClass('is-active')
   })
 
-  it(`doesn't change the "finished reading" to active when "currently reading" link is clicked`, () => {
+  it(`doesn't change the "finished reading" and "add book" links to active when "currently reading" link is clicked`, () => {
     render(
       <Router>
         <Navigation />
@@ -38,8 +38,10 @@ describe('Navigation', () => {
     const links = screen.getAllByRole('link')
     const linkCurrentlyReading = links[0]
     const linkFinishedReading = links[1]
+    const linkAddBook = links[2]
     userEvent.click(linkCurrentlyReading)
     expect(linkFinishedReading).not.toHaveClass('is-active')
+    expect(linkAddBook).not.toHaveClass('is-active')
   })
 
   it('changes to active when "finished reading" link is clicked', () => {
@@ -55,7 +57,7 @@ describe('Navigation', () => {
     expect(linkFinishedReading).toHaveClass('is-active')
   })
 
-  it(`doesn't change the "currently reading" link to active when "finished reading" link is clicked`, () => {
+  it(`doesn't change the "currently reading" and "add book" links to active when "finished reading" link is clicked`, () => {
     render(
       <Router>
         <Navigation />
@@ -65,7 +67,38 @@ describe('Navigation', () => {
     const links = screen.getAllByRole('link')
     const linkCurrentlyReading = links[0]
     const linkFinishedReading = links[1]
+    const linkAddBook = links[2]
     userEvent.click(linkFinishedReading)
     expect(linkCurrentlyReading).not.toHaveClass('is-active')
+    expect(linkAddBook).not.toHaveClass('is-active')
+  })
+
+  it('changes to active when "add book" link is clicked', () => {
+    render(
+      <Router>
+        <Navigation />
+      </Router>
+    )
+
+    const links = screen.getAllByRole('link')
+    const linkAddBook = links[2]
+    userEvent.click(linkAddBook)
+    expect(linkAddBook).toHaveClass('is-active')
+  })
+
+  it(`doesn't change the "currently reading" and "finished reading" links to active when "add book" link is clicked`, () => {
+    render(
+      <Router>
+        <Navigation />
+      </Router>
+    )
+
+    const links = screen.getAllByRole('link')
+    const linkCurrentlyReading = links[0]
+    const linkFinishedReading = links[1]
+    const linkAddBook = links[2]
+    userEvent.click(linkAddBook)
+    expect(linkCurrentlyReading).not.toHaveClass('is-active')
+    expect(linkFinishedReading).not.toHaveClass('is-active')
   })
 })
