cab3520e020392a619a0694867b436d4db26deab
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-26 16:07:04 +0200

    Remove handleAuthorsLength from App

--- src/App.js
+++ src/App.js
@@ -40,21 +40,6 @@ function App({ data }) {
     reader.readAsDataURL(preview)
   }
 
-  function handleAuthorsLength(authors) {
-    if (authors.length === 2) {
-      if (authors[0].includes(authors[1])) {
-        return ` ${authors[0]}`
-      } else {
-        return ` ${authors[0]} and ${authors[1]}`
-      }
-    }
-    if (authors > 2) {
-      return ` ${authors[0]} and others`
-    } else {
-      return ` ${authors}`
-    }
-  }
-
   function handleCreateNewBook(newBookData) {
     const {
       title,
