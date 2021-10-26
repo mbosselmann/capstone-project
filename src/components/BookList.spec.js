51a46bf645cbf770bc2758895b5d69de672a469f
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-27 00:14:48 +0200

    Adjust tests for BookList

--- src/components/BookList.spec.js
+++ src/components/BookList.spec.js
@@ -3,31 +3,21 @@ import BookList from './BookList'
 import placeholder from '../images/placeholder.png'
 
 describe('Book list', () => {
-  const mockOnHandleAuthorsLength = jest.fn()
-
   const books = [
     {
       id: '1',
-      volumeInfo: {
-        title: 'Harry Potter and the Goblet of Fire',
-        authors: ['J. K. Rowling'],
-        imageLinks: {
-          thumbnail: placeholder,
-        },
-      },
+      title: 'Harry Potter and the Goblet of Fire',
+      authors: ['J. K. Rowling'],
+      thumbnail: placeholder,
       finished: false,
       readingSince: '05/2021',
       finishedSince: '',
     },
     {
       id: '2',
-      volumeInfo: {
-        title: 'Cathedral of the Sea',
-        authors: ['Ildefonso Falcones de Sierra'],
-        imageLinks: {
-          thumbnail: placeholder,
-        },
-      },
+      title: 'Cathedral of the Sea',
+      authors: ['Ildefonso Falcones de Sierra'],
+      thumbnail: placeholder,
       finished: true,
       readingSince: '05/2021',
       finishedSince: '08/2021',
@@ -35,39 +25,21 @@ describe('Book list', () => {
   ]
 
   it('renders  one listitem when status is "/library"', () => {
-    render(
-      <BookList
-        books={books}
-        status={'/library'}
-        onHandleAuthorsLength={mockOnHandleAuthorsLength}
-      />
-    )
+    render(<BookList books={books} status={'/library'} />)
 
     const bookListItems = screen.getAllByRole('listitem')
     expect(bookListItems).toHaveLength(1)
   })
 
   it('renders  one listitem when status is not "/library"', () => {
-    render(
-      <BookList
-        books={books}
-        status={'/currently-reading'}
-        onHandleAuthorsLength={mockOnHandleAuthorsLength}
-      />
-    )
+    render(<BookList books={books} status={'/currently-reading'} />)
 
     const bookListItems = screen.getAllByRole('listitem')
     expect(bookListItems).toHaveLength(1)
   })
 
   it('renders no title according to status=""', () => {
-    render(
-      <BookList
-        books={books}
-        status={''}
-        onHandleAuthorsLength={mockOnHandleAuthorsLength}
-      />
-    )
+    render(<BookList books={books} status={''} />)
 
     const titleSection = screen.getByTestId('booklist-title')
     expect(titleSection).toBeEmptyDOMElement()
