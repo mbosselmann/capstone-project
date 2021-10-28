15f70ece42890a9afb433a52c3d1d1c8c78754d7
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-28 12:27:09 +0200

    Adjust tests for AddBookForm, Book and BookList

--- src/components/Book.spec.js
+++ src/components/Book.spec.js
@@ -1,19 +1,22 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter as Router } from 'react-router-dom'
 import Book from './Book'
 import placeholder from '../images/placeholder.png'
 
 describe('Book', () => {
   it('renders book currently read', () => {
     render(
-      <Book
-        key={'1'}
-        title={'Harry Potter and the Goblet of Fire'}
-        authors={['J. K. Rowling']}
-        readingStatus={false}
-        readingStatusDate={'05/2021'}
-        finishedSince={''}
-        bookCover={placeholder}
-      />
+      <Router>
+        <Book
+          key={'1'}
+          title={'Harry Potter and the Goblet of Fire'}
+          authors={['J. K. Rowling']}
+          readingStatus={false}
+          readingStatusDate={'05/2021'}
+          finishedSince={''}
+          bookCover={placeholder}
+        />
+      </Router>
     )
 
     const book = screen.getByRole('listitem')
@@ -22,15 +25,17 @@ describe('Book', () => {
 
   it('renders book that is already read', () => {
     render(
-      <Book
-        key={'2'}
-        title={'Cathedral of the Sea'}
-        authors={['Ildefonso Falcones de Sierra']}
-        readingStatus={true}
-        readingStatusDate={'05/2021'}
-        finishedSince={'08/2021'}
-        bookCover={placeholder}
-      />
+      <Router>
+        <Book
+          key={'2'}
+          title={'Cathedral of the Sea'}
+          authors={['Ildefonso Falcones de Sierra']}
+          readingStatus={true}
+          readingStatusDate={'05/2021'}
+          finishedSince={'08/2021'}
+          bookCover={placeholder}
+        />
+      </Router>
     )
 
     const book = screen.getByRole('listitem')
@@ -39,15 +44,17 @@ describe('Book', () => {
 
   it('renders listitem with correct text', () => {
     render(
-      <Book
-        key={'2'}
-        title={'Cathedral of the Sea'}
-        authors={['Ildefonso Falcones de Sierra']}
-        readingStatus={true}
-        readingStatusDate={'05/2021'}
-        finishedSince={'08/2021'}
-        bookCover={placeholder}
-      />
+      <Router>
+        <Book
+          key={'2'}
+          title={'Cathedral of the Sea'}
+          authors={['Ildefonso Falcones de Sierra']}
+          readingStatus={true}
+          readingStatusDate={'05/2021'}
+          finishedSince={'08/2021'}
+          bookCover={placeholder}
+        />
+      </Router>
     )
 
     expect(screen.getByText('Cathedral of the Sea')).toBeInTheDocument()
@@ -59,15 +66,17 @@ describe('Book', () => {
 
   it('displays image', () => {
     render(
-      <Book
-        key={'2'}
-        title={'Cathedral of the Sea'}
-        authors={['Ildefonso Falcones de Sierra']}
-        readingStatus={true}
-        readingStatusDate={'05/2021'}
-        finishedSince={'08/2021'}
-        bookCover={placeholder}
-      />
+      <Router>
+        <Book
+          key={'2'}
+          title={'Cathedral of the Sea'}
+          authors={['Ildefonso Falcones de Sierra']}
+          readingStatus={true}
+          readingStatusDate={'05/2021'}
+          finishedSince={'08/2021'}
+          bookCover={placeholder}
+        />
+      </Router>
     )
     const displayedImage = screen.getByRole('img')
     expect(displayedImage).toBeInTheDocument(`${placeholder}`)
@@ -75,15 +84,17 @@ describe('Book', () => {
 
   it('displayed image has an alt-text', () => {
     render(
-      <Book
-        key={'2'}
-        title={'Cathedral of the Sea'}
-        authors={['Ildefonso Falcones de Sierra']}
-        readingStatus={true}
-        readingStatusDate={'05/2021'}
-        finishedSince={'08/2021'}
-        bookCover={placeholder}
-      />
+      <Router>
+        <Book
+          key={'2'}
+          title={'Cathedral of the Sea'}
+          authors={['Ildefonso Falcones de Sierra']}
+          readingStatus={true}
+          readingStatusDate={'05/2021'}
+          finishedSince={'08/2021'}
+          bookCover={placeholder}
+        />
+      </Router>
     )
     const altText = screen.getByAltText('bookcover')
     expect(altText).toBeInTheDocument()
