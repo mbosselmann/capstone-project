6506c82ff21498cdb9e883c40dd8d79282a18ce0
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-10-29 16:04:33 +0200

    Change getBook

--- src/services/getBook.js
+++ src/services/getBook.js
@@ -1,12 +1,13 @@
 import formatData from '../utils/formatData'
 
 export default async function getBook(isbn) {
-  return await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
-  )
-    .then(res => res.json())
-    .then(data => formatData(data))
-    .catch(error => {
-      console.error(error)
-    })
+  try {
+    const res = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
+    )
+    const data = await res.json()
+    return formatData(data)
+  } catch (error) {
+    console.error(error)
+  }
 }
