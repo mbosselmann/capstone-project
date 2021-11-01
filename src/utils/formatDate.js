446f74e0f1cfba918c6d93baaafb65f750a4e1a6
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-11-01 21:54:59 +0100

    Adjust getToday and formatDate

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,8 +1,7 @@
 export default function formatDate(date) {
-  const today = date
-  const day = today.toString().slice(8, 10)
-  const month = today.toString().slice(5, 7)
-  const year = today.toString().slice(0, 4)
-  const formatToday = day + '.' + month + '.' + year
-  return formatToday
+  const dateString = date.toString()
+  const day = dateString.slice(8, 10)
+  const month = dateString.slice(5, 7)
+  const year = dateString.slice(0, 4)
+  return day + '.' + month + '.' + year
 }
--- src/utils/getToday.js
+++ src/utils/getToday.js
@@ -1,23 +1,6 @@
 export default function getToday() {
   const date = new Date()
-  function formatDay() {
-    const day = date.getDate().toString()
-    if (day < 10) {
-      const newDay = '0' + day
-      return newDay
-    } else {
-      return day
-    }
-  }
-  function formatMonth() {
-    const month = date.getMonth() + 1
-    if (month < 10) {
-      const newMonth = '0' + month
-      return newMonth
-    } else {
-      return month
-    }
-  }
-  const today = date.getFullYear() + '-' + formatMonth() + '-' + formatDay()
-  return today
+  const dateString = date.toISOString()
+  console.log(dateString)
+  return dateString.slice(0, 10)
 }
